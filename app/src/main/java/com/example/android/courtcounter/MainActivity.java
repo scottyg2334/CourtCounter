4094a3274191f31be86dbf0714909bf69fc6cf9f
Scott Grunwald <sgrunwald@gmail.com>
2017-02-20 14:19:18 -0500

    Added OnClick Funtions to Buttons

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void addThreeForTeamA(View V) {
+        displayForTeamA(3);
+    }
+
+    public void addTwoForTeamA(View V) {
+        displayForTeamA(2);
+    }
+
+    public void addOneForTeamA(View V) {
+        displayForTeamA(1);
+    }
+
 }
