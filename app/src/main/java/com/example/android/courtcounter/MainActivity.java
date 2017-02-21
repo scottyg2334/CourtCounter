2995fc7a0e93aad5e324bfc84c5423ba01090860
Scott Grunwald <sgrunwald@gmail.com>
2017-02-21 18:08:58 -0500

    Added functions for Team B

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -11,10 +11,16 @@ public class MainActivity extends AppCompatActivity {
      */
     // Global Variable for Scores
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     //End of Global Variables
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
     @Override
@@ -22,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+// Team A
     public void addThreeForTeamA(View V) {
         scoreTeamA=scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
@@ -37,5 +43,19 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA=scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
+//Team B
+public void addThreeForTeamB(View V) {
+    scoreTeamB=scoreTeamB + 3;
+    displayForTeamB(scoreTeamB);
+}
 
+    public void addTwoForTeamB(View V) {
+        scoreTeamB=scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View V) {
+        scoreTeamB=scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
 }
